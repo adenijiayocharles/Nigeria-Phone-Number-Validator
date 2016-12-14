7d78583ddf181197bab5d05674af724f54b50c1a
Adeniji Charles ayodipupo <adenijiayocharles@gmail.com>
2016-12-14 13:22:39 +0100

    added IDD code and a lil comments

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
+//Nigerian mobile number prefixes from the four major telcos - MTN, GLO, AIRTEL & ETISALAT
 var mobileNumberPrefix = [0703, 0706, 0803, 0806, 0810, 0813, 0814, 0816, 0903, 0705, 0805, 0811, 0815, 0905, 0701, 0708, 0802, 0808, 0812, 0902, 0809, 0817, 0818, 0909, 0804];
+
+//Nigeria's international dialling code
+var idd = "+234";
 
 var phoneInput = document.getElementById("phone");
 phoneInput.addEventListener("keyup",function(){
@@ -7,5 +11,5 @@ phoneInput.addEventListener("keyup",function(){
 
 	//get length of input
 	var inputLength = phoneInputValue.length;
-	console.log(inputLength);
+	
 });
