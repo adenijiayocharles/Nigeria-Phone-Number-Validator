1cebf4c6a202b9365c9b55a3d84e3fc1de9444ab
Adeniji Charles ayodipupo <adenijiayocharles@gmail.com>
2016-12-14 15:39:23 +0100

    fixed value length count

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
 //Nigerian mobile number prefixes from the four major telcos - MTN, GLO, AIRTEL & ETISALAT
 var mobileNumberPrefix = [0703, 0706, 0803, 0806, 0810, 0813, 0814, 0816, 0903, 0705, 0805, 0811, 0815, 0905, 0701, 0708, 0802, 0808, 0812, 0902, 0809, 0817, 0818, 0909, 0804];
 
+//error div
+var errorDiv = document.getElementById("error");
+
 //Nigeria's international dialling code
 var idd = "+234";
+
+//maximum length of required value
+var maxlength = 14;
 
 var phoneInput = document.getElementById("phone");
 phoneInput.addEventListener("keyup",function(){
@@ -11,5 +17,15 @@ phoneInput.addEventListener("keyup",function(){
 
 	//get length of input
 	var inputLength = phoneInputValue.length;
-	
+
+	//get first four letters of entered value
+	if(inputLength < maxlength){
+		errorDiv.innerHTML = "Invalid length";
+	}else if(inputLength >= 14){
+		errorDiv.innerHTML = "";
+	}
+
+	if(phoneInputValue === ""){
+		errorDiv.innerHTML = "";	
+	}
 });
