e9e0ad1c10d0a77ca1f51b15a0496db533e52e13
Adeniji Charles ayodipupo <adenijiayocharles@gmail.com>
2016-12-19 21:24:00 +0100

    added html error, rewrote variable names

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 //Nigerian mobile number prefixes from the four major telcos - MTN, GLO, AIRTEL & ETISALAT
-var mobileNumberPrefix = [703, 706, 803, 806, 810, 813, 814, 816, 903, 705, 805, 811, 815, 905, 701, 708, 802, 808, 812, 902, 809, 817, 818, 909, 804];
+var telcoPrefixes = [703, 706, 803, 806, 810, 813, 814, 816, 903, 705, 805, 811, 815, 905, 701, 708, 802, 808, 812, 902, 809, 817, 818, 909, 804];
 
 //search array for specific values
 function in_array(value, array){
@@ -16,10 +16,7 @@ var errorDiv = document.getElementById("error");
  
 //phone number text field
 var phoneInput = document.getElementById("phone");
-var dialingCodeFromNumber;
-var prefix;
-var check;
-var idd;
+var dialingCode, mobilePrefix, checkArray;
 
 phoneInput.addEventListener("change",function(){
 
@@ -30,54 +27,97 @@ phoneInput.addEventListener("change",function(){
 	var inputLength = phoneInputValue.length;
 
 	//if length is less than the required length of 14
-	if(inputLength <11){
+	if(inputLength < 11){
 
+		errorDiv.innerHTML = "Invalid gsm number length";
+		errorDiv.className = "invalid";
 		console.log("invalid gsm number length");
 
-	//if length is equal to required length
+	//if length is equal to 11 (070xxxxxxxx)
 	}else if(inputLength === 11){
 
-		prefix = Number(phoneInputValue.substr(1,3));
-		check = in_array(prefix, mobileNumberPrefix);
-		if(check === false){
-			console.log("invalid gsm number");
-		}else{
-			console.log("Valid gsm number");
-		}
+				//get mobile number prefix - 706 or 703 - depending on telco
+				mobilePrefix = Number(phoneInputValue.substr(1,3));
 
+				//check if mobile prefix exists in telcoPrefixes array
+				checkArray = in_array(mobilePrefix, telcoPrefixes);
+				if(checkArray === false){
+					errorDiv.innerHTML = "Invalid gsm number";
+					errorDiv.className = "invalid";			
+					console.log("invalid gsm number");
+				}else{
+					errorDiv.innerHTML = "Valid gsm number";
+					errorDiv.className = "valid";						
+					console.log("Valid gsm number");
+				}
+
+	//if length is equal to 13 (23470xxxxxxxx)
 	}else if(inputLength === 13){
-		prefix = Number(phoneInputValue.substr(3,3));
-		dialingCodeFromNumber = Number(phoneInputValue.substr(0,3));
-		check = in_array(prefix, mobileNumberPrefix);
-		if(check === false){
-			console.log("invalid gsm number");
-		}else if((check >= 0) && (dialingCodeFromNumber === 234)){
-			console.log("Valid gsm number");
-		}else{
-			console.log("invalid gsm number");
-		}
 
+				//get mobile number prefix - 706 or 703 - depending on telco
+				mobilePrefix = Number(phoneInputValue.substr(3,3));
+
+				//get dialling code from mobile number
+				dialingCode = Number(phoneInputValue.substr(0,3));
+
+				//check if mobile prefix exists in telcoPrefixes array		
+				checkArray = in_array(mobilePrefix, telcoPrefixes);
+				if(checkArray === false){
+					
+					errorDiv.innerHTML = "Invalid gsm number";
+					errorDiv.className = "invalid";			
+					console.log("invalid gsm number");
+
+				}else if((checkArray >= 0) && (dialingCode === 234)){
+
+					errorDiv.innerHTML = "Valid gsm number";
+					errorDiv.className = "valid";						
+					console.log("Valid gsm number");
+
+				}else{
+
+					errorDiv.innerHTML = "Invalid gsm number";
+					errorDiv.className = "invalid";			
+					console.log("invalid gsm number");
+
+				}
+
+//if length is equal to 14 (+23470xxxxxxxx)
 	}else if(inputLength === 14){
 
-		//get mobile number prefix from entered vale
-		prefix = Number(phoneInputValue.slice(4,7));
+				//get mobile number prefix from entered value
+				mobilePrefix = Number(phoneInputValue.slice(4,7));
 
-		//get international dialing code from entered value
-		dialingCodeFromNumber = phoneInputValue.slice(0,4);
+				//get dialling code from mobile number - +234
+				dialingCode = phoneInputValue.slice(0,4);
 
-		//check if prefix exists in mobile prefix array
-		check = in_array(prefix, mobileNumberPrefix);
+				//check if prefix exists in mobile prefix array
+				checkArray = in_array(mobilePrefix, telcoPrefixes);
 
-		//if prefix not found in array
-		if(check === false){
-			console.log("invalid gsm number");
-		//if found in array
-		}else if((check >= 0) && (dialingCodeFromNumber === "+234")){
-			console.log("valid gsm number");
-		}else{
-			console.log("invalid gsm number");
-		}
+				//if prefix not found in array
+				if(checkArray === false){
+					
+					errorDiv.innerHTML = "Invalid gsm number";
+					errorDiv.className = "invalid";			
+					console.log("invalid gsm number");
+
+				//if found in array
+				}else if((checkArray >= 0) && (dialingCode === "+234")){
+
+					errorDiv.innerHTML = "Valid gsm number";
+					errorDiv.className = "valid";						
+					console.log("Valid gsm number");
+
+				}else{
+
+					errorDiv.innerHTML = "Invalid gsm number";
+					errorDiv.className = "invalid";			
+					console.log("invalid gsm number");
+
+				}
 	}else if(inputLength > 14){
+		errorDiv.innerHTML = "invalid gsm number length";
+		errorDiv.className = "invalid";			
 		console.log("invalid gsm number length");
 	}
 });
